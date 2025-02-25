b2cc6e1f0f4e61ea46f8521608953252493cd137
mohamedhusman <tharikusman00@gmail.com>
2025-02-25 22:08:50 +0530

    install react js and tailwind css and create the first template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import react from "react";
+
+import "./index.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className="text-purple-500">Hellow word</h1>
+    </div>
+  );
+}
+
+export default App;
